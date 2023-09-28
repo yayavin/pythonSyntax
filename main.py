070c7abcbf48b2d0733af7d24126f289f5488cd0
yayavin <24454981+yayavin@users.noreply.github.com>
2023-09-28 12:38:42 -0500

    Added loops and functions (#1)

--- main.py
+++ main.py
@@ -1,4 +1,48 @@
 import numpy
 
+# Entry point
 if __name__ == '__main__':
     print('Hy, PyCharm')
+
+# Loops
+print("\"for\" loop:")
+for i in range(10):  # From 0 to 9
+    print(i)
+
+print("\"for\" loop with array:")
+arr = ["one", "two", "three"]
+for e in arr:
+    print(e)
+
+print("while loop:")
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
+
+def function(arg):
+    function_message = "message in a function: " + arg
+    print(function_message)
+
+
+print("function invocation:")
+function("arg")
+
+
+def function(*args):
+    for x in args:
+        print(x)
+
+
+print("function with non-fixed number of arguments:")
+function("one", "two", "three")
+
+
+print("examples for types in python:")
+x = str(3)
+y = int(3)
+z = float(3)
+print(type(x))
+print(type(y))
+print(type(z))
